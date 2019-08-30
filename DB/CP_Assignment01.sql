aca45b79fb0f219a4e54a9ac9528ce9899739b15
Aruna Bellgutte Ramesh <44783665+arunabellgutteramesh@users.noreply.github.com>
2019-08-30 03:45:50 +0100

    Initial Commit

---
+++ DB/CP_Assignment01.sql
@@ -0,0 +1,28 @@
+use CP_Schema;
+
+CREATE TABLE IF NOT EXISTS accounts (
+    acc_id INT,
+    acc_name TEXT,
+	acc_balance DOUBLE
+) ;
+
+CREATE TABLE IF NOT EXISTS departments (
+    dept_id INT,
+    dept_name TEXT
+) ;
+
+
+
+select * from departments;
+
+
+select * from accounts;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
